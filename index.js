5285d486a82063136c958ba2d7f3142be960c371
Youri <youribok@msn.com>
2018-06-20 00:48:24 +0200

    Extracts config into separate file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const config = require('./config.json');
 
 const client = new Discord.Client();
 
@@ -7,9 +8,21 @@ client.on('ready', () => {
 });
 
 client.on('message', (msg) => {
-    if (msg.content === 'ping') {
-        msg.channel.send('pong');
+    // const { content: txt, channel: ch, guild } = msg;
+    if (!msg.content.startsWith(config.prefix) || msg.author.bot) {
+        return;
     }
+
+    const args = msg.content.slice(config.prefix.length).split(/\s+/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        msg.reply('pong');
+    } else {
+        msg.reply('ping');
+    }
+
+    console.log('Handled message:', msg.content);
 });
 
-client.login('NDU4NzU2OTQyNjUxODUwNzY1.DgsTdA.rWcAGwcDLYnIZoaKcEMyTd9TjCc');
+client.login(config.token);
