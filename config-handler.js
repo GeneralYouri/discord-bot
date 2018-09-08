64949aff91d9699d94e19d0e83072db0721874c0
Youri <youribok@msn.com>
2018-09-08 16:05:14 +0200

    Removes usage of config.default.js file

--- config-handler.js
+++ config-handler.js
@@ -3,9 +3,8 @@ const fs = require('fs');
 // Read from config from files and apply custom parsing rules
 const loadConfig = function loadConfig() {
     try {
-        const defaultConfig = require('./config.default.json');
-        const environmentConfig = require('./config.json');
-        const config = Object.assign({}, defaultConfig, environmentConfig);
+        /* eslint-disable-next-line global-require */
+        const config = require('./config.json');
 
         config.autoPusers = Array.from(new Set(config.autoPusers));
         config.debtStart = new Date(config.debtStart);
@@ -24,9 +23,9 @@ const storeConfig = function storeConfig() {
     const newConfig = Config;
     newConfig.autoPusers = Array.from(new Set(newConfig.autoPusers));
 
-    const environmentJson = JSON.stringify(newConfig, null, 4);
+    const configJson = JSON.stringify(newConfig, null, 4);
     try {
-        fs.writeFileSync(require.resolve('./config.json'), environmentJson, 'utf-8');
+        fs.writeFileSync(require.resolve('./config.json'), configJson, 'utf-8');
     } catch (e) {
         console.error('Could not write to config file', e);
         throw e;
