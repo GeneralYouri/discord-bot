61d4f2cfc2e3894050b18756916171a585a82124
Youri <youribok@msn.com>
2018-09-21 11:42:38 +0200

    Disables parts of command autop that are meant for higher permission users

--- commands/autop.js
+++ commands/autop.js
@@ -1,7 +1,7 @@
 const { Config, storeConfig } = require('./../config-handler');
 
-const truthies = ['true', 'on', 'enable', 'enabled', 'active', 'activate'];
-const falsies = ['false', 'off', 'disable', 'disabled', 'deactive', 'deactivate'];
+// const truthies = ['true', 'on', 'enable', 'enabled', 'active', 'activate'];
+// const falsies = ['false', 'off', 'disable', 'disabled', 'deactive', 'deactivate'];
 
 const execute = function execute(msg, commandName, type = undefined, ...names) {
     const userSet = new Set(Config.autoPusers);
@@ -17,26 +17,26 @@ const execute = function execute(msg, commandName, type = undefined, ...names) {
         Config.autoPusers = Array.from(userSet);
         storeConfig();
         msg.channel.send('Auto P Dispenser users: ' + Config.autoPusers.join(', '));
-    } else if (type === 'add' || type === 'delete') {
-        names.forEach(name => userSet[type](name));
-        Config.autoPusers = Array.from(userSet);
-        storeConfig();
-        msg.channel.send('Auto P Dispenser users: ' + Config.autoPusers.join(', '));
-    } else if (truthies.includes(type)) {
-        Config.autoP = true;
-        storeConfig();
-        msg.channel.send('Enabled Auto P Dispenser');
-    } else if (falsies.includes(type)) {
-        Config.autoP = false;
-        storeConfig();
-        msg.channel.send('Disabled Auto P Dispenser');
+    // } else if (type === 'add' || type === 'delete') {
+    //     names.forEach(name => userSet[type](name));
+    //     Config.autoPusers = Array.from(userSet);
+    //     storeConfig();
+    //     msg.channel.send('Auto P Dispenser users: ' + Config.autoPusers.join(', '));
+    // } else if (truthies.includes(type)) {
+    //     Config.autoP = true;
+    //     storeConfig();
+    //     msg.channel.send('Enabled Auto P Dispenser');
+    // } else if (falsies.includes(type)) {
+    //     Config.autoP = false;
+    //     storeConfig();
+    //     msg.channel.send('Disabled Auto P Dispenser');
     }
 };
 
 module.exports = {
     name: 'autop',
     description: 'Configure the Auto P Dispenser',
-    usage: '<list|join|quit|add[ user1[ user2]]|delete[ user1[user2]]|on|off>',
+    usage: '<list|join|quit>', // |add[ user1[ user2]]|delete[ user1[user2]]|on|off>',
     cooldown: 5,
     execute,
 };
