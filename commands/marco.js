837400a5ff80a3b495449ae6e5b3cf6f19887e14
GeneralYouri <youribok@msn.com>
2019-05-15 14:52:08 +0200

    Adds command `marco` (polo)

---
+++ commands/marco.js
@@ -0,0 +1,10 @@
+const execute = function execute(message) {
+    message.reply('Polo!');
+};
+
+module.exports = {
+    name: 'marco',
+    description: 'Marco Polo!',
+    usage: '',
+    execute,
+};
