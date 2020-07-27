a8c12939286277df988404104665805ddb92c15a
GeneralYouri <youribok@msn.com>
2020-07-27 20:35:45 +0200

    Updates Config annotations

--- config-handler.js
+++ config-handler.js
@@ -4,13 +4,16 @@ const fs = require('fs');
  * @property {string} token -The Discord Bot token.
  * @property {string} prefix - The command prefix, when a line starts with this prefix the bot will try to match a command to run.
  * @property {string} [devModeGuildID] - A single specific Guild ID, which will be the only guild(server) where the bot responds when running in Dev Mode.
- * @property {[string]} [blacklistCommands] - The command names (not aliases) to disable (will also disable all aliases). A disabled command is treated as non-existant and can't be used.
- * @property {[string]} [hiddenCommands] - The command names (not aliases) to hide (will also hide all aliases). A hidden command can be used normally, but will not show up in the `help` command.
  * @property {number} [defaultCommandCooldown] - The default cooldown for commands that don't have their own custom cooldown. Cooldowns are per-command, AND per-user.
+ * @property {[string]} [hiddenCommands] - The command names (not aliases) to hide (will also hide all aliases). A hidden command can be used normally, but will not show up in the `help` command.
+ * @property {[string]} [blacklistCommands] - The command names (not aliases) to disable (will also disable all aliases). A disabled command can't be used AND won't show up anywhere.
+ * @property {[string]} [blacklistUsers] - User IDs who are not allowed to interact with the bot.
+ * @property {[string]} [adminUsers] - User IDs who have special bot privileges, generally for administrative purposes.
  * @property {boolean} [autoP] - Toggles a special feature that automatically sanitizes messages from specific users by replacing specific characters.
- * @property {string} [autoPusers] - Configures the users for whom the above autoP feature would work, if enabled.
- * @property {number} [debtStart] - The (approximate) time when Djessey's debt interest started counting.
+ * @property {string} [autoPusers] - User IDs for whom to use the autoP feature, if enabled.
  * @property {boolean} [jetlagMode] - Toggles a special feature that accounts for timezone differences with certain users, by reposting their messages in the appropriate time for others.
+ * @property {[string]} [jetlagUsers] - User IDs for whom to use the jetlag feature, if enabled. Also includes the user-specific jetlag time delays to be used.
+ * @property {number} [debtStart] - The (approximate) time when Djessey's debt interest started counting.
  * @property {boolean} [adultMode] - Enables various adult-only (ie not child-safe) features.
  * @property {[string]} [bodyParts] - Body part names are used by fun commands, for example: "x punched y in the face".
  */
