83a02ee45650e4c56ea6753a5c94cf19686d3c1b
GeneralYouri <youribok@msn.com>
2020-07-28 03:27:25 +0200

    Cleans up commands `hug` and `wave`

--- commands/hug.js
+++ commands/hug.js
@@ -1,11 +1,8 @@
 const execute = function execute(message) {
-    let str = message.author;
-
     const userMentions = message.mentions.users;
     const target = (userMentions.size && userMentions.first() !== message.author) ? userMentions.first() : 'themselves';
-    str += ` hugs at ${target}`;
 
-    message.channel.send(str);
+    message.channel.send(`${message.author.username} hugs ${target}`);
 };
 
 module.exports = {
