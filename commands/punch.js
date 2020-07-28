4752caa351da1f3b623acda22c0f4d16988d18b4
GeneralYouri <youribok@msn.com>
2020-07-28 03:30:05 +0200

    Cleans up commands `kiss`, `punch`, `slap`

--- commands/punch.js
+++ commands/punch.js
@@ -1,11 +1,9 @@
 const { Config } = require('../config-handler.js');
 
 const execute = function execute(message) {
-    let str = message.author;
-
     const userMentions = message.mentions.users;
     const target = (userMentions.size && userMentions.first() !== message.author) ? userMentions.first() : 'themselves';
-    str += ` punches ${target}`;
+    let str = `${message.author.username} punches ${target}`;
 
     if (Array.isArray(Config.bodyParts) && Config.bodyParts.length > 1) {
         const bodyPart = Config.bodyParts[Math.floor(Math.random() * Config.bodyParts.length)];
